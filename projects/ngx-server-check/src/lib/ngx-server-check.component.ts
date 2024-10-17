440bcb6f58a28c26a096a2a480111119cc596084
AbduBehindTheCode <abderrahmen.trigui95@gmail.com>
2024-10-17 23:09:24 +0200

    v1.0.0

--- projects/ngx-server-check/src/lib/ngx-server-check.component.ts
+++ projects/ngx-server-check/src/lib/ngx-server-check.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { apiStatus } from './ngx-server-check.type';
 
 @Component({
   selector: 'lib-ngx-server-check',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: 'ngx-server-check.component.html',
-  styleUrl: 'ngx-server-check.component.html'
+  styleUrl: 'ngx-server-check.component.scss'
 })
-export class NgxServerCheckComponent {}
+export class NgxServerCheckComponent implements OnInit{
+  apiStatus: apiStatus[] = [];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.data.subscribe((data: any) => {
+      this.apiStatus = data.response; 
+    });
+  }
+}
